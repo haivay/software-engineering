b3e218294ab43d9081864ae89f4e317e1635848b
Кирилл Хайрутдинов <79259641+haivay@users.noreply.github.com>
2021-03-22 04:09:45 +0500

    Update task1.js

--- labrab02/task1.js
+++ labrab02/task1.js
@@ -4,10 +4,11 @@ const ut = require('./ut01');
 let students = ut.csv_to_json('./csv/students.csv');
 
 let avgAge = (arr) => {
-    let res = _.ceil(_
-        .map(arr, (x) => +x.age)
-        .reduce((acc, next) => acc += next/arr.length, 0)
-    , 2);
+    let res = _
+        .floor(_
+            .map(arr, (x) => +x.age)
+            .reduce((acc, next) => acc += next/arr.length, 0)
+        , 0);
     return res;
 }
 
