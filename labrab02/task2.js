feb9ad27634d07b532516b43da61a4ad5212d99c
Кирилл Хайрутдинов <79259641+haivay@users.noreply.github.com>
2021-03-22 04:10:32 +0500

    Update task2.js

--- labrab02/task2.js
+++ labrab02/task2.js
@@ -3,20 +3,19 @@ const _ = require('lodash');
 const ut = require('./ut01');
 
 let students = ut.csv_to_json('./csv/students.csv');
-let groups = ut.csv_to_json('./csv/groups.csv')
+let groups = ut.csv_to_json('./csv/groups.csv');
 let group = "ПИб-1";
 
 let avgAgeForGroup = (group) => {
-    let groupId = _.filter(groups, {'nameGr': group}) 
-    let clonedStudents = _.cloneDeep(students);
-    let studentsOfGroup = _.filter(clonedStudents, {'idGr': groupId[0].id});
+    let groupId = _.find(groups, {'nameGr': group}).id;
+    let studentsOfGroup = _.filter(students, {'idGr': groupId});
     
     let res = _
-        .ceil(_
+        .floor(_
             .map(studentsOfGroup, (x) => +x.age)
             .reduce((acc, next) => acc += next/studentsOfGroup.length, 0)
-        , 2);
+        , 0);
     return res;
 }
 
-console.log(avgAgeForGroup(group)); //19.5
+console.log(`Средний возраст группы ${group} = ${avgAgeForGroup(group)} лет`);
